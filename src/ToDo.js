0a3618ef09da2666a69e65f363ae6f15356797c9
Akshay Kotha <akotha12@gmail.com>
2023-01-11 13:35:48 -0500

    Basic Component Information Added

---
+++ src/ToDo.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function TodosComponent() {
+    return (
+        <div>
+            <h1>Hello from our Todo component!</h1>
+        </div>
+    );
+}
+
+export default TodosComponent;
